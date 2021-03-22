8e6acae5e5c4b18193efc9f438c65723317c6b42
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-03-22 10:29:22 +0100

    typeof operator

--- codecademy/js.variables/main.js
+++ codecademy/js.variables/main.js
@@ -1,4 +1,4 @@
-let myName = 'Tim';
-let myCity = 'Innsbruck';
-
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
