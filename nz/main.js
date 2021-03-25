141a56f0fc888bf470fed407cf690b5bac9c0b8e
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-03-25 16:16:40 +0100

    tilelayer

--- nz/main.js
+++ nz/main.js
@@ -1,4 +1,11 @@
 
 
-const map = L.map("map");
+const map = L.map("map", {
+    center [-42.118611, 171.326944]
+    zoom: 13
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+
 console.log(document.querySelector("#map"));
