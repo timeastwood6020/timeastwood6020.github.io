dca58100d72864db9b47a27fd8dce11958b10b10
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-03-25 16:38:07 +0100

    let stop

--- nz/main.js
+++ nz/main.js
@@ -1,7 +1,14 @@
 
-
+let stop = {
+    nr: 16,
+    name: "Pancake Rocks"
+    lat: -42.118611
+    long: 171.326944
+    user: "timeastwood6020"
+    wikipedia: "https://de.wikipedia.org/wiki/Pancake_Rocks"
+};
 const map = L.map("map", {
-    center [-42.118611, 171.326944]
+    center [stop.lat, stop.long]
     zoom: 13
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
