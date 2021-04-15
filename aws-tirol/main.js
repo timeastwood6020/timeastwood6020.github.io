9ba68f4d70caedf3ee2a0e4d9c64599ffe951a47
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-15 17:12:19 +0200

    fitbounds

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,4 +1,3 @@
-
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
 let map = L.map("map", {
@@ -11,28 +10,35 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
-    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
-    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
     "BasemapAT.overlay+ortho": L.layerGroup([
         L.tileLayer.provider('BasemapAT.orthofoto'),
-        L.tileLayer.provider("BasemapAT.overlay")
+        L.tileLayer.provider('BasemapAT.overlay')
     ])
 }).addTo(map);
 
-let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson"
 
-fetch(awsUrl).then(response => response.json())
-.then(json =>  {
-    console.log("Daten konvertiert: ", json());
-for (station of json.features) {
-    console.log("Station: ", station);
-    let marker = L.marker(
-        [station.geometry.coordinates[1],
-    station.geometry.coordinates[2] ]
-    );
-    marker.addTo(map);
-}
-});
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
+let awsLayer = L.featureGroup();
+layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
+awsLayer.addTo(map);
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            // console.log('Station: ', station);
+            let marker = L.marker([
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0]
+            ]);
+            marker.bindPopup(`<h3>${station.properties.name}</h3>`);
+            marker.addTo(awsLayer);
+        }
+
+        map.fitBounds(awsLayer.getBounds());
+        
+    });
