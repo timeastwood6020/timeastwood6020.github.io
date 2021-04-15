14ae8f1d0de3a96c3154b07a1a38d8408535658b
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-15 17:50:35 +0200

    schneehöhe

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -23,7 +23,11 @@ let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
-awsLayer.addTo(map);
+// awsLayer.addTo(map);
+let snowLayer = L.featureGroup();
+layerControl.addOverlay(snowLayer, "Schneehöhen");
+snowLayer.addTo(map);
+
 
 fetch(awsUrl)
     .then(response => response.json())
@@ -46,9 +50,21 @@ fetch(awsUrl)
               <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
               <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
             </ul>
-            <a href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag${station.properties.plot}.png">Grafik</a>
+            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(snowLayer);
+            }
         }
         // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
