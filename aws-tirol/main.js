2dadc3a85f8f2942ce016247d72554e7e8a50993
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-15 16:43:04 +0200

    stationen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -11,5 +11,28 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
 }).addTo(map);
+
+let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson"
+
+fetch(awsUrl).then(response => response.json())
+.then(json =>  {
+    console.log("Daten konvertiert: ", json());
+for (station of json.features) {
+    console.log("Station: ", station);
+    let marker = L.marker(
+        [station.geometry.coordinates[1],
+    station.geometry.coordinates[2] ]
+    );
+    marker.addTo(map);
+}
+});
+
