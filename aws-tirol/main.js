1343c0a880d5e396ad7bca4a63eebad37773cbea
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-15 17:20:31 +0200

    datum auf deutsch

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,3 +1,33 @@
+Skip to content
+Why GitHub? 
+Team
+Enterprise
+Explore 
+Marketplace
+Pricing 
+Search
+
+Sign in
+Sign up
+webmapping
+/
+webmapping.github.io
+00
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+webmapping.github.io/aws-tirol/main.js /
+@oeggl
+oeggl add locale date formatting
+Latest commit 14939eb 10 seconds ago
+ History
+ 1 contributor
+50 lines (44 sloc)  1.53 KB
+  
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
 let map = L.map("map", {
@@ -35,17 +65,16 @@ fetch(awsUrl)
                 station.geometry.coordinates[1],
                 station.geometry.coordinates[0]
             ]);
+            let formattedDate = new Date(station.properties.date);
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul>
-            <li>Datum: ${station.proporties.date}</li>
-            <li>Temperatur: ${station.proporties.LT} C</li>
+              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+              <li>Temperatur: ${station.properties.LT} C</li>
             </ul>
-
             `);
             marker.addTo(awsLayer);
         }
-
+        // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
-
     });
