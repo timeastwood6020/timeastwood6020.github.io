4f4c2aa3cceb6e1b49baafe5898b81b6e48afa91
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-15 10:20:19 +0200

    main.js update 15.04

--- nz/main.js
+++ nz/main.js
@@ -1,56 +1,61 @@
-
 let stop = {
     nr: 16,
     name: "Pancake Rocks",
     lat: -42.118611,
-    long: 171.326944,
+    lng: 171.326944,
     user: "timeastwood6020",
     wikipedia: "https://de.wikipedia.org/wiki/Pancake_Rocks",
-};
+    
+}
+
+
 const map = L.map("map", {
-    center [stop.lat, stop.lng]
-    zoom: 13
+    // center: [stop.lat, stop.lng],
+    // zoom: 13,
     layers: [
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
 });
 
-let nav = document.querySelector("#navigation");
-console.log(nav);
+let nav = document.querySelector('#navigation');
+console.log('Navigation HTML Element: ', nav);
+// console.log(ROUTE);
 
-ROUTE.sort((stop1, stop2) =>{
-    return.stop1.nr > stop2.nr
+ROUTE.sort((stop1, stop2) => {
+    if (stop1.nr > stop2.nr) {
+        return 1;
+    } else {
+        return -1;
+    }
 });
+for (let entry of ROUTE) {
+    // console.log(entry);
 
+    nav.innerHTML += `<option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>`;
+    let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+    mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
+<p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
+`);
 
+    if (entry.nr == 22) {
+        map.setView([entry.lat, entry.lng], 13);
+        mrk.openPopup();
+    }
 
-console.log(document.querySelector("#map"));
-for (let entry of ROUTE){
-    console.log(entry);
-
-    nav.innerHTML +=
-    <option value="${entry.user}">Stop</option>
-let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
-mrk.bindPopup("
-<h4>Stop ${stop.nr}: ${stop.name}</h4>
-<p><a href="${stop.wikipedia}">Read about Stop in Wikipedia</a></p>
-);
-if (entry.nr==16) {
-    map.setView([entry.lat, entry.lng],13)
-    mrk.openPopup();
-}
 }
 
-console.log(document.querySelector("#map"));
-
-nav.options.selectedIndex = 22-1
-nav.onchange = (evt) =>{
+nav.selectedIndex = 22 - 1;
+nav.onchange = (evt) => {
+    console.log(evt.target.selectedIndex);
     let selected = evt.target.selectedIndex;
-    let options = evt.target.options
-    let value = options[selected].value;
-    let link= "https://${username}.github.io//nz/index.html;
-    console.log(username, link);
+    let options = evt.target.options;
 
+    let username = options[selected].value;
+    let link = `https://${timeastwood6020}.github.io/nz/index.html`;
     window.location.href = link;
+    console.log(link);
+}
 
-};
+console.log(document.querySelector("#map"));
+
+
