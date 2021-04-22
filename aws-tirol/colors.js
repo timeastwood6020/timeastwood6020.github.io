1ca6e76c9d19101c2eba37c62454fa175fbe4921
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-22 09:21:59 +0200

    temperature 0-50 grad C

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -54,8 +54,36 @@ const COLORS = {
         min: -5,
         max: 0,
         col: "#8fffff"
+    }, {
+        min: 0,
+        max: 5,
+        col: "#b0ffbc"
+    }, {
+        min: 5,
+        max: 10,
+        col: "#ffff73"
+    }, {
+        min: 10,
+        max: 15,
+        col: "#ffbe7d"
+    }, {
+        min: 15,
+        max: 20,
+        col: "#ff9b41"
+    }, {
+        min: 20,
+        max: 25,
+        col: "#ff5a41"
+    }, {
+        min: 25,
+        max: 30,
+        col: "#ff1e23"
+    }, {
+        min: 30,
+        max: 50,
+        col: "#fa3c96"
+    }],
 
-    ],
     snowheight: [
 
     ]
