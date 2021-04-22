3aed594e38fbebe9f804682108795b2d7cc03f00
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-04-22 10:28:46 +0200

    leaflet links in main.js einfügen ???

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -99,4 +99,30 @@ fetch(awsUrl)
         map.fitBounds(awsLayer.getBounds());
     });
 
-    
+    //https://leafletjs.com/reference-1.7.1.html#featuregroup
+    let awsLayer = L.featureGroup();
+layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
+
+//https://leafletjs.com/reference-1.7.1.html#layer
+var layer = L.marker(latlng).addTo(map);
+layer.addTo(map);
+layer.remove();
+
+//L.tilelyer
+// https://leafletjs.com/reference-1.7.1.html#gridlayer
+
+
+//L.map
+
+//L.control.layers
+// https://leafletjs.com/reference-1.7.1.html#control
+
+//L.divIcon
+// https://leafletjs.com/reference-1.7.1.html#divicon
+var myIcon = L.divIcon({className: 'my-div-icon'});
+// you can set .my-div-icon styles in CSS
+
+
+//L.marker
+// https://leafletjs.com/reference-1.7.1.html#marker
+
