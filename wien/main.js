0e6a9dd64ead0e2a0c6d65207c55e7950414091e
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-05-06 17:41:04 +0200

    icons

--- wien/main.js
+++ wien/main.js
@@ -158,7 +158,24 @@ var miniMap = new L.Control.MiniMap(L.tileLayer.provider("BasemapAT.basemap"),{
 
 5b3ce3597851110001cf6248fb6f58176de0477f944b4c79efdeb7bb
 
+// Reachability plugin
 L.control.reachability({
     // add settings/options here
-    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    apiKey: '5b3ce3597851110001cf6248fb6f58176de0477f944b4c79efdeb7bb'
+    drawButtonContent: '',
+    drawButtonStyleClass: 'fa fa-pencil-alt',
+    deleteButtonContent: '',
+    deleteButtonStyleClass: 'fa fa-trash',
+    distanceButtonContent: '',
+    distanceButtonStyleClass: 'fa fa-road',
+    timeButtonContent: '',
+    timeButtonStyleClass: 'fa fa-clock',
+    travelModeButton1Content: '',
+    travelModeButton1StyleClass: 'fa fa-car',
+    travelModeButton2Content: '',
+    travelModeButton2StyleClass: 'fa fa-bicycle',
+    travelModeButton3Content: '',
+    travelModeButton3StyleClass: 'fa fa-male',
+    travelModeButton4Content: '',
+    travelModeButton4StyleClass: 'fa fa-wheelchair'
 }).addTo(map);
