efb7d3d410f2259b3b44453f63cc58c3abedba8e
timeastwood6020 <timothy.eastwood@student.uibk.ac.at>
2021-05-20 16:08:49 +0200

    gpx loaded

--- Tirol/main.js
+++ Tirol/main.js
@@ -42,3 +42,26 @@ let layerControl = L.control.layers({
 
 // Overlay mit GPX-Track anzeigen
 overlays.tracks.addTo(map);
+
+const drawTrack = (nr) => {
+    // console.log('Track: ', nr);
+    let gpxTrack = new L.GPX(`tracks/${nr}.gpx`, {
+        async: true,
+        marker_options: {
+            startIconUrl: `icons/number_${nr}.png`,
+            endIconUrl: 'icons/finish.png',
+            shadowUrl: null,
+        },
+        polyline_options: {
+            color: 'black',
+            dashArray: [2, 5],
+        },
+    }).addTo(overlays.tracks);
+    gpxTrack.on("loaded", () => {
+        console.log('loaded gpx');
+        map.fitBounds(gpxTrack.getBounds());
+    })
+};
+
+const selectedTrack = 7;
+drawTrack(selectedTrack);
